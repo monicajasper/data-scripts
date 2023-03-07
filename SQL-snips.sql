1fe54e0962e851493c9aec486922824424015256
Monica Jasper <35152870+monicajasper@users.noreply.github.com>
2023-03-07 14:07:52 +1300

    remove gis vis ex from data mung sql snips

--- SQL-snips.sql
+++ SQL-snips.sql
@@ -1,13 +1,6 @@
 /*SQL snips for def queries in GIS*/
 
 
-
---Purpose: Return data in interval of 5
---Use Case: Helpful for contour lines or labels
-MOD(CVALUE,5) = 0
-
-
-
 --Purpose: Determine IDs / Record #s in one table but not in other
 --Use Case: Useful for seeing which data has been filtered out of joined tables
 
